8ae3e71f27b1b358b39ea515a86857d351ca42ef
Martin Vysny <mavi@vaadin.com>
2023-03-24 18:57:55 +0200

    initial commit

---
+++ bin/renogy_client.dart
@@ -0,0 +1,5 @@
+import 'package:renogy_client/renogy_client.dart' as renogy_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${renogy_client.calculate()}!');
+}
---
+++ test/renogy_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:renogy_client/renogy_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
